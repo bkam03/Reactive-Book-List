5c967ef4efb3c56ca22502833d743f2db8ecaf83
BK <bkam03@yahoo.com>
2017-08-24 21:48:18 -1000

    basic booklist file works

---
+++ src/components/BookListAppTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookList = ( props ) => {
+  return (
+    <div className="BookListAppTitle">
+      this is the book list.
+    </div>
+  );
+}
+
+export default BookList;
