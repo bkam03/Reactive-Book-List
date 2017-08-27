e9eb9e6b1378757dd999cc761dee77a543e7eea7
BK <bkam03@yahoo.com>
2017-08-26 15:32:39 -1000

    booklist uses book module and feeds in db data

---
+++ src/containers/BookList/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import Book from '../../components/Book.js'
+
+class BookList extends Component {
+
+  render(){
+    let books = this.props.books;
+    console.log( 'booklist');
+    return (
+      <ul className="BookListContainer">
+        {
+          books.map(( book ) => {
+            return(
+              <Book
+                title={book.title}
+                author={book.author}
+              />
+            )
+        } )
+        }
+      </ul>
+    );
+  }
+};
+
+export default BookList;
