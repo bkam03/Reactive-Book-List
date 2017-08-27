850dafad64c2de14c109a5258fa28b9a57dc97f2
BK <bkam03@yahoo.com>
2017-08-26 17:11:42 -1000

    new book form working

--- src/containers/NewBookForm/index.js
+++ src/containers/NewBookForm/index.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 
 class NewBookForm extends Component {
   render(){
-
+    let props = this.props;
     return(
       <div className="book-form">
         <input
           type="text"
           name="title"
           placeholder="Book Title"
-
+          onChange={ props.titleChange }
         />
         <br />
         <input
           type="text"
           name="author"
           placeholder="Book Author"
+          onChange={ props.authorChange }
         />
         <br />
-        <button>Submit Book</button>
+        <button onClick= {props.submitBook}>Submit Book</button>
       </div>
     );
   };
